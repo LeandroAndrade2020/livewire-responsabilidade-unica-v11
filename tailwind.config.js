bbcd93794f54934ca29e58a84c085bef70d73a96
‘LeandroAndrade’ <profeleandro@outlook.com>
2024-03-12 15:59:29 -0300

    fix: layout e add talstakui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [
+        require('./vendor/tallstackui/tallstackui/tailwind.config.js')
+    ],
+
     content: [
+        './vendor/tallstackui/tallstackui/src/**/*.php',
+        
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',

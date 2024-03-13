e3eb79d1c31e9f5e83bdbb6f118941846d594514
‘LeandroAndrade’ <profeleandro@outlook.com>
2024-03-13 07:13:07 -0300

    feat: class e migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PdfController;
+use App\Livewire\{Escola, Estagiario, User};
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +16,12 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('users-acrtivity', User\ActivityIndex::class)->name('users.activity.index');
+
+    Route::get('user', User\Index::class)->name('user.index');
+    Route::get('escola', Escola\Index::class)->name('escola.index');
+    Route::get('estagiario', Estagiario\Index::class)->name('estagiario.index');
+
+    Route::get('pdf-view/{estagiario}', PdfController::class)->name('pdf.view');
 });

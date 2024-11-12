698d114e2ac0a40e1b9f51bf7abce74ecd9bafe2
‘LeandroAndrade’ <profeleandro@outlook.com>
2024-11-12 17:09:43 -0300

    add: create

--- app/Livewire/Cargo/Create.php
+++ app/Livewire/Cargo/Create.php
@@ -2,10 +2,32 @@
 
 namespace App\Livewire\Cargo;
 
+use App\Livewire\Forms\CargoForm;
 use Livewire\Component;
+use TallStackUi\Traits\Interactions;
 
 class Create extends Component
 {
+    use Interactions;
+
+    public CargoForm $form;
+
+    public $modalCreate = false;
+
+    public function save()
+    {
+        $this->validate();
+        $this->form->store();
+
+        $this->toast()->success('Cadastro realizado com sucesso!')->send();
+
+        $this->dispatch('dispatch-cargo-create-save')->to(Table::class);
+
+        $this->modalCreate = false;
+
+        return redirect()->route('cargo.index');
+    }
+
     public function render()
     {
         return view('livewire.cargo.create');
--- app/Livewire/Escola/Create.php
+++ app/Livewire/Escola/Create.php
@@ -2,12 +2,39 @@
 
 namespace App\Livewire\Escola;
 
+use App\Livewire\Forms\EscolaForm;
+use Illuminate\Support\Facades\{Gate, Log};
 use Livewire\Component;
+use Symfony\Component\HttpFoundation\Response;
+use TallStackUi\Traits\Interactions;
 
 class Create extends Component
 {
+    use Interactions;
+
+    public EscolaForm $form;
+
+    public $modalCreate = false;
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->form->store();
+
+        $this->toast()->success('Cadastro realizado com sucesso!')->send();
+
+        $this->dispatch('dispatch-escola-create-save')->to(Table::class);
+
+        $this->modalCreate = false;
+
+        return redirect()->route('escola.index');
+    }
+
     public function render()
     {
+        abort_if(Gate::denies('diretor_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
         return view('livewire.escola.create');
     }
 }

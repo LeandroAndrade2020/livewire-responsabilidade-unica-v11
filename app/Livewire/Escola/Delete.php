259e7f1853f896df82cdcfa489405cbaa219640c
‘LeandroAndrade’ <profeleandro@outlook.com>
2024-11-12 17:12:37 -0300

    add: delete

--- app/Livewire/Escola/Delete.php
+++ app/Livewire/Escola/Delete.php
@@ -2,10 +2,47 @@
 
 namespace App\Livewire\Escola;
 
+use App\Models\Escola;
+use Illuminate\Support\Facades\Gate;
+use Livewire\Attributes\{Locked, On};
 use Livewire\Component;
+use Symfony\Component\HttpFoundation\Response;
+use TallStackUi\Traits\Interactions;
 
 class Delete extends Component
 {
+    use Interactions;
+
+    #[Locked]
+    public $id;
+
+    #[Locked]
+    public $name;
+
+    public $modalDelete = false;
+
+    #[On('dispatch-escola-table-delete')]
+    public function set_escola($id, $name)
+    {
+        $this->id   = $id;
+        $this->name = $name;
+
+        $this->modalDelete = true;
+    }
+
+    public function del()
+    {
+        $del = Escola::destroy($this->id);
+
+        ($del)
+        ? $this->toast()->success('Cadastro excluído com sucesso!')->send()
+        : $this->toast()->error('Cadastro não excluído!')->send();
+
+        $this->modalDelete = false;
+
+        $this->dispatch('dispatch-escola-delete-del')->to(Table::class);
+    }
+
     public function render()
     {
         return view('livewire.escola.delete');
